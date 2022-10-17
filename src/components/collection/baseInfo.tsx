8bba38e49c0cf042196ee2142e882a04f676bd35
weixuefeng <weixuefeng@diynova.com>
2022-10-17 14:50:49 +0800

    fix: show chain info

--- src/components/collection/baseInfo.tsx
+++ src/components/collection/baseInfo.tsx
@@ -1,9 +1,9 @@
 /*
  * @Author: zhuxiaotong zhuxiaotong@diynova.com
  * @Date: 2022-09-29 15:46:19
- * @LastEditors: zhuxiaotong zhuxiaotong@diynova.com
- * @LastEditTime: 2022-10-09 16:42:17
- * @FilePath: /wave-chinese-website/src/components/index/baseInfo.tsx
+ * @LastEditors: weixuefeng weixuefeng@diynova.com
+ * @LastEditTime: 2022-10-17 14:50:21
+ * @FilePath: /wave-china-h5/src/components/collection/baseInfo.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import React, { useEffect, useState } from 'react'
@@ -17,7 +17,6 @@ function BaseInfo(props) {
     <div className="baseinfo-wrap">
       <p className="title">
         {collectionInfo.name}
-        <span className="icon">EVT</span>
       </p>
       <div className="nums">
         <div className="nums-item">
