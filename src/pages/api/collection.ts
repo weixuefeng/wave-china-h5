fa49aa76bd847fd32343c9facc7596ade3b21a96
weixuefeng <weixuefeng@diynova.com>
2022-10-24 15:00:24 +0800

    fix: api config

--- src/pages/api/collection.ts
+++ src/pages/api/collection.ts
@@ -1,9 +1,9 @@
 /*
  * @Author: error: git config user.name && git config user.email & please set dead value or install git
  * @Date: 2022-10-08 11:55:00
- * @LastEditors: liukeke liukeke@diynova.com
- * @LastEditTime: 2022-10-09 20:44:36
- * @FilePath: /wave-chinese-website/src/pages/api/collection.ts
+ * @LastEditors: weixuefeng weixuefeng@diynova.com
+ * @LastEditTime: 2022-10-24 15:00:04
+ * @FilePath: /wave-china-h5/src/pages/api/collection.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
@@ -17,7 +17,7 @@ type Data = {
   country: string
 }
 
-const url = `${WAVE_BASE_URL}/api/v5/nft/mystery-box/detail`
+const url = `${WAVE_BASE_URL}/api/v6/nft/mystery-box/detail`
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   axios
