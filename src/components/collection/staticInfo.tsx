1415f6206f77887b25b07823b0b79c4ea75ee346
weixuefeng <weixuefeng@diynova.com>
2022-10-27 20:20:55 +0800

    fix: 41

--- src/components/collection/staticInfo.tsx
+++ src/components/collection/staticInfo.tsx
@@ -1,8 +1,8 @@
 /*
  * @Author: zhuxiaotong zhuxiaotong@diynova.com
  * @Date: 2022-09-29 15:46:19
- * @LastEditors: zhuxiaotong zhuxiaotong@diynova.com
- * @LastEditTime: 2022-10-27 13:36:48
+ * @LastEditors: weixuefeng weixuefeng@diynova.com
+ * @LastEditTime: 2022-10-27 20:20:16
  * @FilePath: /wave-china-h5/src/components/collection/staticInfo.tsx
  * @LastEditors: weixuefeng weixuefeng@diynova.com
  * @LastEditTime: 2022-10-08 20:54:29
@@ -124,10 +124,10 @@ function StaticInfo(props) {
             )}
         </div>
         <div className="spec-tip">
-          <img src="/assets/image/icon-point.png" alt="" className="icon-point" />
+          {/* <img src="/assets/image/icon-point.png" alt="" className="icon-point" /> */}
           {/* className={showAllTips ? '' : 'h-8 line-clamp-2'} */}
           <p>
-            {t('TIPS')}
+            {/* {t('TIPS')} */}
             {/* <a className="more" href="https://www.newtonproject.org/en/evt/">
               {t('MORE')}
             </a> */}
