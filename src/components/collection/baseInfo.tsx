432fdb6687f21f6bdf71290ba4422a7161af413e
weixuefeng <weixuefeng@diynova.com>
2022-10-27 11:43:55 +0800

    fix: words

--- src/components/collection/baseInfo.tsx
+++ src/components/collection/baseInfo.tsx
@@ -2,7 +2,7 @@
  * @Author: zhuxiaotong zhuxiaotong@diynova.com
  * @Date: 2022-09-29 15:46:19
  * @LastEditors: weixuefeng weixuefeng@diynova.com
- * @LastEditTime: 2022-10-17 14:50:21
+ * @LastEditTime: 2022-10-27 11:43:03
  * @FilePath: /wave-china-h5/src/components/collection/baseInfo.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -22,7 +22,7 @@ function BaseInfo(props) {
           <span className="number">{collectionInfo.box_total - collectionInfo.sold_quantity}</span>
         </div>
         <div className="nums-item">
-          <span className="tip">{t('TOTALSUPPLY')}</span>
+          <span className="tip">{t('CURRENT_RELASE')}</span>
           <span className="number">{collectionInfo.box_total}</span>
         </div>
       </div>
